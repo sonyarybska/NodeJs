c5dccb62e77f5bf7b08ec91b1fda0084ad6df215
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-10-06 01:18:51 +0300

    fixed

--- homework/app.js
+++ homework/app.js
@@ -1,13 +1,13 @@
-const express=require('express');
-const userRouter=require('./routers/user.router');
+const express = require('express');
+const userRouter = require('./routers/user.router');
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use('/users',userRouter);
+app.use('/users', userRouter);
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('App listen 5000');
 });
--- homework/routers/user.router.js
+++ homework/routers/user.router.js
@@ -1,11 +1,11 @@
-const router=require('express').Router();
+const router = require('express').Router();
 
-const userControllers=require('../controllers/user.conrollers');
+const userControllers = require('../controllers/user.conrollers');
 
 router.get('/', userControllers.getUsers);
-router.post('/',userControllers.postUser);
+router.post('/', userControllers.postUser);
 
 router.get('/:id', userControllers.getUser);
 router.delete('/:id', userControllers.deleteUser);
 
-module.exports=router;
+module.exports = router;
