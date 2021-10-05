89402e356ad60d8bfc1dd72f6f6e841c88bf5b9f
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-10-05 23:42:06 +0300

    lesson2

--- homework/controllers/user.conrollers.js
+++ homework/controllers/user.conrollers.js
@@ -6,29 +6,34 @@ const dbLink = path.join(__dirname, '..', 'users.json');
 const readFile = () => fs.readFile(dbLink);
 const writeFile = (data) => fs.writeFile(dbLink, data);
 
+const parseData=(data)=>{
+    return JSON.parse(data.toString());
+}
+
 module.exports = {
     getUsers: async (req, res) => {
         const users = await readFile();
-
         res.json(JSON.parse(users.toString()));
     },
     getUser: async (req, res) => {
         const users = await readFile();
-        const parse = JSON.parse(users.toString());
+        const parse = parseData(users);
 
-        res.json(parse[req.params.id]);
+        res.json(parse[req.params.id-1]);
     },
     postUser: async (req, res) => {
         const users = await readFile();
-        const parse = JSON.parse(users.toString());
+        const parse = parseData(users);
 
         parse.push({...req.body, id: parse.length + 1});
+
+        await writeFile(JSON.stringify(parse));
 
         res.end();
     },
     deleteUser: async (req, res) => {
         const users = await readFile();
-        const parse = JSON.parse(users.toString());
+        const parse = parseData(users);
 
         parse.splice(+req.params.id - 1, 1);
         parse.map((value, index) => value.id = index + 1);
