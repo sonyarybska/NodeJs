77dd369a95269f58f2216c3acfc8296ab1a3c7ad
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-10-05 23:25:08 +0300

    lesson2

--- homework/controllers/user.conrollers.js
+++ homework/controllers/user.conrollers.js
@@ -1,9 +1,10 @@
-const fs = require('fs/promises');
 const path = require('path');
+const fs = require('fs/promises');
+
 const dbLink = path.join(__dirname, '..', 'users.json');
 
 const readFile = () => fs.readFile(dbLink);
-const writeFile = (data) => fs.writeFile(dbLink,data);
+const writeFile = (data) => fs.writeFile(dbLink, data);
 
 module.exports = {
     getUsers: async (req, res) => {
@@ -18,13 +19,22 @@ module.exports = {
         res.json(parse[req.params.id]);
     },
     postUser: async (req, res) => {
-        const users=await readFile();
-        const parse=JSON.parse(users.toString());
+        const users = await readFile();
+        const parse = JSON.parse(users.toString());
 
-        parse.push({...req.body,id:parse.length+1});
+        parse.push({...req.body, id: parse.length + 1});
+
+        res.end();
+    },
+    deleteUser: async (req, res) => {
+        const users = await readFile();
+        const parse = JSON.parse(users.toString());
+
+        parse.splice(+req.params.id - 1, 1);
+        parse.map((value, index) => value.id = index + 1);
 
         await writeFile(JSON.stringify(parse));
 
         res.end();
     }
-}
+};
--- homework/routers/user.router.js
+++ homework/routers/user.router.js
@@ -2,7 +2,8 @@ const router=require('express').Router();
 const userControllers=require('../controllers/user.conrollers');
 
 router.get("/", userControllers.getUsers);
-router.get("/:id", userControllers.getUser);
 router.post("/",userControllers.postUser);
+router.get("/:id", userControllers.getUser);
+router.delete("/:id", userControllers.deleteUser);
 
 module.exports=router;
