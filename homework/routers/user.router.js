ae5568607d0d7c4d46f0f63049e154c16609c454
feden2906 <feden2906@gmail.com>
2021-10-05 23:59:04 +0300

    fix

--- homework/routers/user.router.js
+++ homework/routers/user.router.js
@@ -1,8 +1,10 @@
 const router=require('express').Router();
+
 const userControllers=require('../controllers/user.conrollers');
 
 router.get('/', userControllers.getUsers);
 router.post('/',userControllers.postUser);
+
 router.get('/:id', userControllers.getUser);
 router.delete('/:id', userControllers.deleteUser);
 
