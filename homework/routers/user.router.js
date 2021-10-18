873f717cea74b832aee48f5b45bb41fb69a0c1c5
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-10-18 18:08:28 +0300

    lesson6

--- homework/routers/user.router.js
+++ homework/routers/user.router.js
@@ -1,16 +1,28 @@
 const router = require('express').Router();
 
-const {userMiddleware,authMiddleware} = require('../middlewares');
+const {userMiddleware, authMiddleware} = require('../middlewares');
 const {userControllers} = require('../controllers');
 const {userValidators: {createUserValidator, updateUserValidator}} = require('../validators');
 const {userRole: {ADMIN, MANAGER}} = require('../constans');
 
 router.get('/', userControllers.getUsers);
-router.post('/', userMiddleware.isBodyValid(createUserValidator), userMiddleware.createUserMiddleware, userControllers.postUser);
 
-router.get('/:id', userMiddleware.isUserIdValid, userMiddleware.checkExistUser, userControllers.getUser);
-router.put('/:id', userMiddleware.isUserIdValid, userMiddleware.isBodyValid(updateUserValidator),
-    userMiddleware.checkExistUser, userControllers.updateUser);
+router.post('/',
+    userMiddleware.isBodyValid(createUserValidator),
+    userMiddleware.createUserMiddleware,
+    userControllers.postUser);
+
+router.get('/:id',
+    userMiddleware.isUserIdValid,
+    userMiddleware.checkExistUser,
+    userControllers.getUser);
+
+router.put('/:id',
+    userMiddleware.isUserIdValid,
+    userMiddleware.isBodyValid(updateUserValidator),
+    userMiddleware.checkExistUser,
+    authMiddleware.checkAccessToken,
+    userControllers.updateUser);
 
 router.delete('/:id',
     userMiddleware.isUserIdValid,
