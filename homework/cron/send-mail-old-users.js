94ee6f25a6173ad3efe3d44d21ebeb1c71e354ee
sonyarybska <80405651+sonyarybska@users.noreply.github.com>
2021-10-29 20:53:31 +0300

    lesson11

--- homework/cron/send-mail-old-users.js
+++ homework/cron/send-mail-old-users.js
@@ -8,12 +8,13 @@ const {emailService} = require('../services');
 dayJs.extend(utc);
 
 module.exports = async () => {
-    const tenDays = dayJs.utc().subtract(1, 'minute');
+    const tenDays = dayJs.utc().subtract(10, 'day');
 
     const user = await OAuthSchema.find({createdAt: {$lt: tenDays}}).populate('user_id');
 
     if (user.length) {
 
+        // eslint-disable-next-line max-len
         const send = user.map(value => emailService(value.user_id.email, emailTemplatesEnum.REMIND, {userName: value.user_id.name}));
 
         Promise.allSettled(send).then(result => console.log(result));
